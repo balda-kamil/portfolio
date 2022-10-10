7dab35f32401e94d7ff83f4fe7d97a95a6f6ad51
Kamil Balda <baldakamil@gmail.com>
2022-10-10 22:18:56 +0200

    content changes

---
+++ config.js
@@ -0,0 +1,18 @@
+const pageConfig = { 
+  footer: {
+    hideMenu: true
+  }, 
+  articles: {
+    hideArticles: true,
+    hideinMenu: true
+  },
+  photos: {
+    hidePhotos: true
+  },
+  articles: {
+    hideArticles: true
+  },
+  RSS: false
+}
+
+export default pageConfig
